50fc4f4d2f2d110637cda1a0015884d3f654cfc9
wentingzoe <ywtzoe@gmail.com>
2022-05-19 12:54:58 -0400

    basic layout of css and two components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer } from './components';
+import './App.css';
+
+const apiKey = 'cnzcdd7qbbvs';
+const client = StreamChat.getInstance(apiKey);
 
 function App() {
   return (
-    <div>
-      <h1>Medical Chat</h1>
+    <div className='app__wrapper'>
+      <Chat client={client} theme='team light'>
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   )
 }
