4382ec0db9fe6a6d0fcc3ed8e897a2e202bfbe5c
wentingzoe <ywtzoe@gmail.com>
2022-05-25 16:10:38 -0400

    Update Channel Container and create 4 chirlden components: ChannelInner, CreateChannel, EditChannel, TeamMessage

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { Channel, useChatContext } from 'stream-chat-react';
+import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from './';
+
 
 const ChannelContainer = () => {
   return (
