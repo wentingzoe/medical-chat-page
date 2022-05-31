402232638117d80431239dee0f637b4764edf376
wentingzoe <ywtzoe@gmail.com>
2022-05-31 00:22:24 -0400

    add command in TCP

--- client/src/components/TeamChannelPreview.jsx
+++ client/src/components/TeamChannelPreview.jsx
@@ -3,22 +3,25 @@ import { Avatar, useChatContext } from 'stream-chat-react';
 
 const TeamChannelPreview = ({ setActiveChannel, setIsCreating, setIsEditing, setToggleContainer, channel, type }) => {
     const { channel: activeChannel, client } = useChatContext();
-
+    //rename channel to activeChannel
     const ChannelPreview = () => (
         <p className="channel-preview__item">
             # {channel?.data?.name || channel?.data?.id}
         </p>
+        //specify the channel name, add ?.data?.name to make sure it's not undefined || channel don't have the name 
     );
+    //* functional component use const x = ()=>(), which is instant return;
 
 
     const DirectPreview = () => {
         const members = Object.values(channel.state.members).filter(({ user }) => user.id !== client.userID);
-    
+        //cannot use channel.state.members since it's not a real object, so we use Object.values to give values of the specific object
+        //.filter(({user}) => user.id !== client.userID) To map over all the users and filter out the current user (logged in user)
         console.log(members[0]);
 
         return (
             <div className="channel-preview__item single">
-                <Avatar 
+                <Avatar
                     image={members[0]?.user?.image}
                     name={members[0]?.user?.fullName || members[0]?.user?.id}
                     size={24}
@@ -34,14 +37,14 @@ const TeamChannelPreview = ({ setActiveChannel, setIsCreating, setIsEditing, set
                 ? 'channel-preview__wrapper__selected'
                 : 'channel-preview__wrapper'
         }
-        onClick={() => {
-            setIsCreating(false);
-            setIsEditing(false);
-            setActiveChannel(channel);
-            if(setToggleContainer) {
-                setToggleContainer((prevState) => !prevState)
-            }
-        }}
+            onClick={() => {
+                setIsCreating(false);
+                setIsEditing(false);
+                setActiveChannel(channel);
+                if (setToggleContainer) {
+                    setToggleContainer((prevState) => !prevState)
+                }
+            }}
         >
             {type === 'team' ? <ChannelPreview /> : <DirectPreview />}
         </div>
